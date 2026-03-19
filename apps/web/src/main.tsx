2a72f6d21802827b4e9a13caf8243d27696c09a5
lucadisarno <cosmicoluca@gmail.com>
2026-03-19 10:28:37 +0100

    feat: task 7.7 - calendar custom styling

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import '@/styles/calendar.css'
 import './index.css'  
 
 createRoot(document.getElementById("root")!).render(
